5fcc0f654aeaa0d489f64cac87075d403aa874af
emigu041 <emigu041@student.liu.se>
2023-09-06 11:50:10 +0200

    Almost done just somm cookies left

--- Uppgift1/test.js
+++ Uppgift1/test.js
@@ -5,16 +5,16 @@ function publishMessage()
     const message = document.getElementById('message').value;
     const errorMessageDiv = document.getElementById('error-message');
 
-
     console.log("gehh")
     if (author.length == 0)
     {
         console.log("hej")
-        author = "Jon Doe"
+        author = "John Doe"
     }
 
     if (message.length == 0 || message.length > 140) 
     {
+        
         errorMessageDiv.style.display = 'block';
         errorMessageDiv.innerText = 'Meddelandet måste vara mellan 1 och 140 tecken långt.';
         return;
@@ -25,6 +25,8 @@ function publishMessage()
 
     const timestamp = new Date().toLocaleString();
     const messageObject = {author, message, timestamp}
+    //setCookie(author, message, timestamp);
+    //getCookies();
 
     clearFields();
     showMessage(messageObject);
@@ -54,11 +56,41 @@ function showMessage(messageObject)
     cardFooter.className = 'card-footer';
     cardFooter.innerText = messageObject.timestamp;
 
+    //------------------------------------------
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'read-checkbox';
+    checkbox.addEventListener('change', toggleCheckbox);
+
+    const checkboxText = document.createElement('lable');
+    checkboxText.innerText = 'Har läst';
+    checkboxText.insertBefore(checkbox, checkboxText.firstChild);
+
+    cardHeader.appendChild(checkboxText);
+    //------------------------------------------
+
+
     card.appendChild(cardHeader);
     card.appendChild(cardBody);
     card.appendChild(cardFooter);
 
     container.prepend(card); //ÄNDRAD RAD, appendChild() -> prepend(). Lägger nyaste inlägg först genom att lägga den "först i child-listan".
+
+}
+
+function toggleCheckbox(event)
+{
+
+    const card = event.target.closest('.card');
+    
+    if (event.taget.checked)
+    {
+        card.classList.add('read');
+    }else
+    {
+        card.classList.remove('read');
+    }
+
 }
 
 function clearFields() 
@@ -66,3 +98,22 @@ function clearFields()
     document.getElementById('author').value = '';
     document.getElementById('message').value = '';
 }
+
+// function setCookie(author, message, time) {
+//     if getCookie
+//     console.log("asdlkföjasdölf");
+//     const d = new Date();
+//     d.setTime(d.getTime() + (24*60*60*1000));
+//     let expires = "expires=" + d.toUTCString();
+//     const payload = [author, message, time];
+//     document.cookie = time + "=" + payload + ";" + expires + ";path=/";
+// }
+
+// function getCookies() {
+//     cookies = document.cookie.split(";")
+//     console.log(cookies)
+
+//     console.log("hej")
+// }
+
+// hej;jag;heter;oskar
